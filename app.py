d0f019894d95e8386bafac409be99887541b02c0
zageabb <zageabb@users.noreply.github.com>
2026-09-03 16:28:30 +0100

    Bump Tender Designer to v0.1.2

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ from services.tender_monitor import start_tender_monitor_worker
 login_manager = LoginManager()
 csrf = CSRFProtect()
 migrate = Migrate()
-DEFAULT_APP_VERSION = "0.1.1"
+DEFAULT_APP_VERSION = "0.1.2"
 
 
 def create_app(config_overrides: dict | None = None) -> Flask:
